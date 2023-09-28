e11681813e84ef6ae4920294f7ae0feec3b4feae
stli8113 <stli8113@colorado.edu>
2023-09-28 13:34:16 -0600

    lab 5 start

---
+++ Labs/Lab 5/newton_example.py
@@ -0,0 +1,108 @@
+# import libraries
+import numpy as np
+        
+def driver():
+#f = lambda x: (x-2)**3
+#fp = lambda x: 3*(x-2)**2
+#p0 = 1.2
+
+  f = lambda x: (x-2)*(x-5)*np.exp(x)
+  fp = lambda x: (x-2)*(x-5)*np.exp(x)+(2*x-7)*np.exp(x) 
+  p0 = 1.2
+
+  Nmax = 100
+  tol = 1.e-14
+
+  (p,pstar,info,it) = newton(f,fp,p0,tol, Nmax)
+  print('the approximate root is', '%16.16e' % pstar)
+  print('the error message reads:', '%d' % info)
+  print('Number of iterations:', '%d' % it)
+
+
+def newton(f,fp,p0,tol,Nmax):
+  """
+  Newton iteration.
+  
+  Inputs:
+    f,fp - function and derivative
+    p0   - initial guess for root
+    tol  - iteration stops when p_n,p_{n+1} are within tol
+    Nmax - max number of iterations
+  Returns:
+    p     - an array of the iterates
+    pstar - the last iterate
+    info  - success message
+          - 0 if we met tol
+          - 1 if we hit Nmax iterations (fail)
+     
+  """
+  p = np.zeros(Nmax+1);
+  p[0] = p0
+  for it in range(Nmax):
+      p1 = p0-f(p0)/fp(p0)
+      p[it+1] = p1
+      if (abs(p1-p0) < tol):
+          pstar = p1
+          info = 0
+          return [p,pstar,info,it]
+      p0 = p1
+  pstar = p1
+  info = 1
+  return [p,pstar,info,it]
+
+def bisection(f,fp,fp2,a,b,tol):
+    
+#    Inputs:
+#     f,fp,a,b       - function, derivatives and endpoints of initial interval
+#      tol  - bisection stops when interval length < tol
+
+#    Returns:
+#      astar - approximation of root
+#      ier   - error message
+#            - ier = 1 => Failed
+#            - ier = 0 == success
+
+#     first verify there is a root we can find in the interval 
+
+    fa = f(a)
+    fb = f(b)
+    if (fa*fb>0):
+       ier = 1
+       astar = a
+       return [astar, ier]
+
+#   verify end points are not a root 
+    if (fa == 0):
+      astar = a
+      ier =0
+      return [astar, ier]
+
+    if (fb ==0):
+      astar = b
+      ier = 0
+      return [astar, ier]
+
+    count = 0
+    d = 0.5*(a+b)
+    fd2 = f(d)*fp2(d) / fp(d)**2
+    while (abs(fd2) > 1):
+      fd = f(d)
+      if (fd ==0):
+        astar = d
+        ier = 0
+        return [astar, ier]
+      if (fa*fd<0):
+         b = d
+      else: 
+        a = d
+        fa = fd
+      d = 0.5*(a+b)
+      fd2 = f(d)*fp2(d) / fp(d)**2
+      count = count +1
+#      print('abs(d-a) = ', abs(d-a))
+      
+    astar = d
+    ier = 0
+    return [astar, ier]
+        
+driver()
