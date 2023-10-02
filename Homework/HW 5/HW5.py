cf2633b4e4b3b4014e43f91512c06290f512303e
stli8113 <stli8113@colorado.edu>
2023-10-02 10:00:04 -0600

    hw5 p1 work

--- Homework/HW 5/HW5.py
+++ Homework/HW 5/HW5.py
@@ -12,27 +12,33 @@ def driver():
     x0 = 1
     y0 = 1
 
-    xstar, ystar, ier = LazyNewton2d(f,g,J,x0,y0, Nmax, tol)
+    points, xstar, ystar, ier = LazyNewton2d(f,g,J,x0,y0, Nmax, tol)
     print("Root found at: ", [xstar, ystar])
     print("Error message:", ier)
+    
     
     return
 
 
 def LazyNewton2d(f,g,J, x0, y0, Nmax, tol):
     count = 0
+    iterates = np.array([[x0,y0],])
+
     while (count < Nmax):
         x1 = x0 - J[0,0]*f(x0,y0) - J[0,1]*g(x0,y0)
         y1 = y0 - J[1,0]*f(x0,y0) - J[1,1]*g(x0,y0)
+        iterates = np.append(iterates, [[x1,y1]],axis=0)
+        # print(iterates)
+        # print(x1,y1)
         if (abs(np.linalg.norm([x0,y0]) - np.linalg.norm([x1,y1])) < tol):
             ier = 0
-            return [x1,y1, ier]
+            return [iterates, x1,y1, ier]
         x0 = x1
         y0 = y1
 
     xstar = x1
     ystar = y1
     ier = 1
-    return [xstar, ystar, ier]
+    return [iterates, xstar, ystar, ier]
 
 driver()
