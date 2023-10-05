5c869cae05b0ecdee53e20f14647ad1355557677
stli8113 <stli8113@colorado.edu>
2023-10-05 11:37:30 -0600

    hw5 final

--- Homework/HW 5/HW5.py
+++ Homework/HW 5/HW5.py
@@ -9,6 +9,11 @@ def driver():
     gx = lambda x,y: 3*y**2 - 3*x**2
     gy = lambda x,y: 6*x*y 
 
+    f1 = lambda x,y,z: x**2 + 4*y**2 + 4*z**2 - 16
+    fx1 = lambda x,y,z: 2*x
+    fy1 = lambda x,y,z: 8*y
+    fz1 = lambda x,y,z: 8*z
+
     J = np.array([[1/6, 1/18],[0,1/6]])
     
     Nmax = 100
@@ -16,6 +21,7 @@ def driver():
 
     x0 = 1
     y0 = 1
+    z0 = 1
 
     points, xstar, ystar, ier = LazyNewton2d(f,g,J,x0,y0, Nmax, tol)
     print("Root found at: ", [xstar, ystar])
@@ -34,6 +40,11 @@ def driver():
     errors = abs(norms - norms[-1])
 
     print(np.log(errors[-2]) / np.log(errors[-3]))
+
+    points, xstar, ystar, zstar, ier = gradient3d(x0,y0,z0,f1,fx1,fy1,fz1,tol,Nmax)
+    print("Root found at: ", [xstar, ystar, zstar])
+    print("Error message:", ier)
+    print("Root test returns:", f1(xstar, ystar, zstar))
     # print(errors)
     # plt.figure(1)
     # plt.plot(np.log(errors[0:-2]),np.log(errors[1:-1]))
@@ -79,4 +90,27 @@ def newton2d(f,g,fx,fy,gx,gy,x0,y0,Nmax,tol):
         y0 = y1
     ier =1
     return [iterates, x1,y1, ier]
+def gradient3d(x0,y0,z0,f,fx,fy,fz, tol, Nmax):
+    count = 0
+    iterates = np.array([[x0,y0,z0],])
+    while (count < Nmax):
+        fatx = f(x0,y0,z0)
+        fxatx = fx(x0,y0,z0)
+        fyatx = fy(x0,y0,z0)
+        fzatx = fz(x0,y0,z0)
+        d = fatx/(fxatx**2 + fyatx**2 + fzatx**2)
+
+        x1 = x0 - fxatx*d
+        y1 = y0 - fyatx*d
+        z1 = z0 - fzatx*d
+        iterates = np.append(iterates, [[x1,y1,z1]],axis=0)
+
+        if (abs(np.linalg.norm([x0,y0,z0]) - np.linalg.norm([x1,y1,z1])) < tol):
+            ier = 0
+            return [iterates, x1,y1,z1, ier]
+        x0 = x1
+        y0 = y1
+        z0 = z1
+    ier = 1
+    return [iterates, x1,y1,z1, ier]
 driver()
