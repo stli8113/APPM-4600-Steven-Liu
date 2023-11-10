6b1d9f21783455394dcd882c0ff546f5b147d29f
stli8113 <stli8113@colorado.edu>
2023-11-10 12:42:01 -0700

    hw10 work

--- Homework/HW 10/HW10.py
+++ Homework/HW 10/HW10.py
@@ -1,16 +1,39 @@
 import numpy as np
 from scipy.integrate import quad
+import matplotlib.pyplot as plt
 def driver():
     f = lambda x: 1 / (1 + x**2)
     a = -5
     b = 5
 
-    y, err, infodict= quad(f, a, b, full_output=1)
-    # y, err, infodict, _= quad(f, a, b, full_output=1, epsabs=10**-4)
+    f1 = lambda x: np.sin(x)
+    McL = lambda x: abs(f1(x) - 1 - x**3/6 + x**5/120)
+    P33 = lambda x: abs(f1(x) - (x - 7*x**3/60) / (1 + x**2/20))
+    P42 = lambda x: abs(f1(x) - (x) / (1 + x**2/6 + 7*x**4/120))
+    P24 = lambda x: abs(f1(x) - (x - 7*x**3/60) / (1 + x**2/20))
+    
+    xeval = np.linspace(0,5,100)
+    mcErr = McL(xeval)
+    P33Err = P33(xeval)
+    P42Err = P42(xeval)
+    P24Err= P24(xeval)
+
+    plt.plot(xeval, mcErr, "b", label="MacLaurin Error")
+    plt.plot(xeval, P33Err, "y", label="Cubic Error")
+    plt.plot(xeval, P42Err, "g", label="P_2^4 Error")
+    plt.plot(xeval, P24Err, "k", label="P_4^2 Error")
+    plt.title("Comparison of Approximation Errors")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.legend()
+    plt.show()
+
+    # y, err, infodict= quad(f, a, b, full_output=1)
+    y, err, infodict= quad(f, a, b, full_output=1, epsabs=10**-4)
     print(infodict["neval"])
-    print(y)
-    print(trapz(a,b,1291, f))
-    print(simpsons(a,b,108,f))
+    # print(y)
+    # print(trapz(a,b,1291, f))
+    # print(simpsons(a,b,108,f))
     return
 
 
